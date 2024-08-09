f803ccbc24b91871a36623d7875e4358b4ba4479
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-08-09 11:45:52 -0400

    init back and frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from './components/Nav/Nav.jsx'
+import MainContainer from './components/MainContainer/MainContainer.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import './App.css'
+
+const App = () => {
+    return <>
+        <Nav />
+        <MainComponent />
+        <Footer />
+    </>
+}
+
+export default App
---
+++ frontend/src/components/MainContainer/MainContainer.jsx
@@ -0,0 +1,9 @@
+import './MainContainer.css'
+
+const MainContainer = () => {
+    return <>
+        
+    </>
+}
+
+export default MainContainer
