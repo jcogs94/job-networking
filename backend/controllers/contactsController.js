33148100a304459d0fa3569b1319202ce125e769
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-08-11 12:18:00 -0400

    update route for contacts

--- backend/controllers/contactsController.js
+++ backend/controllers/contactsController.js
@@ -50,6 +50,36 @@ contactsRouter.post('/', async (req, res) => {
     }
 })
 
+// Update contact
+contactsRouter.put('/:contactId', async (req, res) => {
+    try {
+        let foundCompany = await Company.findById(req.companyId)
+        
+        if (!foundCompany) {
+            res.status(404)
+            throw new Error('Company not found.')
+        } else {
+            for (let i = 0; i < foundCompany.contacts.length; i++) {
+                if (foundCompany.contacts[i]._id.toString() === req.params.contactId) {
+                    let updateKeys = Object.keys(req.body)
+                    updateKeys.forEach((key) => {
+                        if (key === 'initialContactDate') {
+                            // TODO: if not same as current, update numbered days from initialContact
+                        } else {
+                            foundCompany.contacts[i][key] = req.body[key]
+                        }
+                    })
+                }
+            }
+
+            foundCompany.save()
+            res.status(200).json(foundCompany)
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
 // Delete contact
 contactsRouter.delete('/:contactId', async (req, res) => {
     try {
